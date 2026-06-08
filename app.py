4297a01a1c4b7bf7c1dd46bcc08d0dd55b400af5
MikeMike-K <mkachin3@mail.ru>
2026-06-08 13:30:27 +0300

    Increased password_hash length for scrypt

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ db = SQLAlchemy(app)
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    password_hash = db.Column(db.String(120), nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
     role = db.Column(db.String(20), default='student')  # 'student' или 'teacher'
 
     def set_password(self, password):
